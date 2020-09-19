73c184423ed47dc457953131be55318fc380a6ea
Östman Alexander <alexander.ostman@sweco.se>
2020-09-19 20:40:24 +0200

    Made it so the npm package is actually built on release.
    Also added lib folder in ignore

--- typescript-client/lib/decoders/ScalarDecoder.d.ts
+++
@@ -1,2 +0,0 @@
-import { Scalar } from "../generated/koralium_pb";
-export default function decodeScalar(scalar: Scalar): string | number | boolean | undefined;
--- typescript-client/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./client";
-export * from "./queryBuilder";
-export * from "./objectToSelectMapper";
