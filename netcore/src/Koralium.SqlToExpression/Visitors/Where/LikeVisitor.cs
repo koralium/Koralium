8b267d9b751fabcfb4a9669f43c5295b86cbd710
Östman Alexander <alexander.ostman@sweco.se>
2020-10-25 16:27:04 +0100

    Refactored like visitor to reduce complexity.

--- netcore/src/Koralium.SqlToExpression/Visitors/Where/LikeVisitor.cs
+++ netcore/src/Koralium.SqlToExpression/Visitors/Where/LikeVisitor.cs
@@ -64,136 +64,155 @@ namespace Koralium.SqlToExpression.Visitors.Where
             public Expression Expression { get; set; }
         }
 
+        private VisitResult VisitBinaryExpression_Internal(Microsoft.SqlServer.TransactSql.ScriptDom.BinaryExpression binaryExpression)
+        {
+            var leftResult = VisitInternal(binaryExpression.FirstExpression);
+
+            //Left result should only contain: '%text' or 'text'
+            if (leftResult.StartsWith && !leftResult.WildcardOnly)
+            {
+                throw new SqlErrorException("Like expression can only support starts with ('text%'), ends with ('%text') and contains ('%text%') at this time");
+            }
+
+            var rightResult = VisitInternal(binaryExpression.SecondExpression);
+
+            //Right result should only contain 'text%' or 'text'
+            if (rightResult.EndsWith && !rightResult.WildcardOnly)
+            {
+                throw new SqlErrorException("Like expression can only support starts with ('text%'), ends with ('%text') and contains ('%text%') at this time");
+            }
+
+            if (binaryExpression.BinaryExpressionType != BinaryExpressionType.Add)
+            {
+                throw new SqlErrorException("Like expression can only do string concatination at this time");
+            }
+
+            //If one of the results are null, they have been removed since they were just '%'
+            //We only return the other result since concat is not required
+            if (leftResult.Expression == null)
+            {
+                return rightResult;
+            }
+
+            //Remove empty strings that was just for wildcarding
+            if (leftResult.WildcardOnly && rightResult.WildcardOnly)
+            {
+                return new VisitResult()
+                {
+                    EndsWith = true,
+                    StartsWith = true,
+                    WildcardOnly = true,
+                    Expression = Expression.Constant("")
+                };
+            }
+            else if (leftResult.WildcardOnly)
+            {
+                return new VisitResult()
+                {
+                    EndsWith = true,
+                    StartsWith = rightResult.StartsWith,
+                    Expression = rightResult.Expression
+                };
+            }
+            else if (rightResult.WildcardOnly)
+            {
+                return new VisitResult()
+                {
+                    StartsWith = true,
+                    Expression = leftResult.Expression,
+                    EndsWith = leftResult.EndsWith
+                };
+            }
+
+            var expression = BinaryUtils.CreateBinaryExpression(leftResult.Expression, rightResult.Expression, BinaryExpressionType.Add);
+
+            return new VisitResult()
+            {
+                StartsWith = rightResult.StartsWith || rightResult.WildcardOnly,
+                EndsWith = leftResult.EndsWith || leftResult.WildcardOnly,
+                Expression = expression
+            };
+        }
+        
+        private VisitResult VisitStringLiteral_Internal(StringLiteral stringLiteral)
+        {
+            var value = stringLiteral.Value;
+
+            if (value.Equals("%"))
+            {
+                return new VisitResult()
+                {
+                    WildcardOnly = true,
+                    StartsWith = true,
+                    EndsWith = true,
+                    Expression = Expression.Constant("")
+                };
+            }
+
+            bool startsWith = false;
+            bool endsWith = false;
+            if (value.StartsWith("%"))
+            {
+                endsWith = true;
+                value = value.Substring(1);
+            }
+            if (value.EndsWith("%") && (value.Length - 1 == 0 || value[value.Length - 1] != '\\'))
+            {
+                startsWith = true;
+                value = value.Substring(0, value.Length - 1);
+            }
+
+            return new VisitResult()
+            {
+                EndsWith = endsWith,
+                StartsWith = startsWith,
+                Expression = Expression.Constant(value)
+            };
+        }
+
+        private VisitResult VisitVariableReference_Internal(VariableReference variableReference)
+        {
+            if (!_visitorMetadata.Parameters.TryGetParameter(variableReference.Name, out var parameter))
+            {
+                throw new SqlErrorException($"The parameter {variableReference.Name} could not be found, did you have include @ before the parameter name?");
+            }
+
+            return new VisitResult()
+            {
+                Expression = parameter.GetValueAsExpression()
+            };
+        }
+
+        private VisitResult VisitColumnReferenceExpression_Internal(ColumnReferenceExpression columnReferenceExpression)
+        {
+            var identifiers = columnReferenceExpression.MultiPartIdentifier.Identifiers.Select(x => x.Value).ToList();
+
+            identifiers = MemberUtils.RemoveAlias(_previousStage, identifiers);
+            var memberAccess = MemberUtils.GetMember(_previousStage, identifiers, out var property);
+            AddUsedProperty(property);
+            return new VisitResult()
+            {
+                Expression = memberAccess
+            };
+        }
+
         private VisitResult VisitInternal(ScalarExpression scalarExpression)
         {
             if(scalarExpression is Microsoft.SqlServer.TransactSql.ScriptDom.BinaryExpression binaryExpression)
             {
-                var leftResult = VisitInternal(binaryExpression.FirstExpression);
-
-                //Left result should only contain: '%text' or 'text'
-                if (leftResult.StartsWith && !leftResult.WildcardOnly)
-                {
-                    throw new SqlErrorException("Like expression can only support starts with ('text%'), ends with ('%text') and contains ('%text%') at this time");
-                }
-
-                var rightResult = VisitInternal(binaryExpression.SecondExpression);
-
-                //Right result should only contain 'text%' or 'text'
-                if (rightResult.EndsWith && !rightResult.WildcardOnly)
-                {
-                    throw new SqlErrorException("Like expression can only support starts with ('text%'), ends with ('%text') and contains ('%text%') at this time");
-                }
-
-                if(binaryExpression.BinaryExpressionType != BinaryExpressionType.Add)
-                {
-                    throw new SqlErrorException("Like expression can only do string concatination at this time");
-                }
-
-                //If one of the results are null, they have been removed since they were just '%'
-                //We only return the other result since concat is not required
-                if(leftResult.Expression == null)
-                {
-                    return rightResult;
-                }
-
-                //Remove empty strings that was just for wildcarding
-                if(leftResult.WildcardOnly && rightResult.WildcardOnly)
-                {
-                    return new VisitResult()
-                    {
-                        EndsWith = true,
-                        StartsWith = true,
-                        WildcardOnly = true,
-                        Expression = Expression.Constant("")
-                    };
-                }
-                else if (leftResult.WildcardOnly)
-                {
-                    return new VisitResult()
-                    {
-                        EndsWith = true,
-                        StartsWith = rightResult.StartsWith,
-                        Expression = rightResult.Expression
-                    };
-                }
-                else if (rightResult.WildcardOnly)
-                {
-                    return new VisitResult()
-                    {
-                        StartsWith = true,
-                        Expression = leftResult.Expression,
-                        EndsWith = leftResult.EndsWith
-                    };
-                }
-
-                var expression = BinaryUtils.CreateBinaryExpression(leftResult.Expression, rightResult.Expression, BinaryExpressionType.Add);
-
-                return new VisitResult()
-                {
-                    StartsWith = rightResult.StartsWith || rightResult.WildcardOnly,
-                    EndsWith = leftResult.EndsWith || leftResult.WildcardOnly,
-                    Expression = expression
-                };
+                return VisitBinaryExpression_Internal(binaryExpression);
             }
             else if(scalarExpression is StringLiteral stringLiteral)
             {
-                var value = stringLiteral.Value;
-
-                if (value.Equals("%"))
-                {
-                    return new VisitResult()
-                    {
-                        WildcardOnly = true,
-                        StartsWith = true,
-                        EndsWith = true,
-                        Expression = Expression.Constant("")
-                    };
-                }
-
-                bool startsWith = false;
-                bool endsWith = false;
-                if (value.StartsWith("%"))
-                {
-                    endsWith = true;
-                    value = value.Substring(1);
-                }
-                if (value.EndsWith("%") && (value.Length - 1 == 0 || value[value.Length - 1] != '\\'))
-                {
-                    startsWith = true;
-                    value = value.Substring(0, value.Length - 1);
-                }
-                
-                return new VisitResult()
-                {
-                    EndsWith = endsWith,
-                    StartsWith = startsWith,
-                    Expression = Expression.Constant(value)
-                };
+                return VisitStringLiteral_Internal(stringLiteral);
             }
             else if (scalarExpression is VariableReference variableReference)
             {
-                
-                if (!_visitorMetadata.Parameters.TryGetParameter(variableReference.Name, out var parameter))
-                {
-                    throw new SqlErrorException($"The parameter {variableReference.Name} could not be found, did you have include @ before the parameter name?");
-                }
-
-                return new VisitResult()
-                {
-                    Expression = parameter.GetValueAsExpression()
-                };
+                return VisitVariableReference_Internal(variableReference);
             }
             else if (scalarExpression is ColumnReferenceExpression columnReferenceExpression)
             {
-                var identifiers = columnReferenceExpression.MultiPartIdentifier.Identifiers.Select(x => x.Value).ToList();
-
-                identifiers = MemberUtils.RemoveAlias(_previousStage, identifiers);
-                var memberAccess = MemberUtils.GetMember(_previousStage, identifiers, out var property);
-                AddUsedProperty(property);
-                return new VisitResult()
-                {
-                    Expression = memberAccess
-                };
+                return VisitColumnReferenceExpression_Internal(columnReferenceExpression);
             }
             else
             {
