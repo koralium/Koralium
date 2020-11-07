3cb46d7cef971464ecd5b4e7246a9b784ad1f2f7
Östman Alexander <alexander.ostman@sweco.se>
2020-11-07 19:30:41 +0100

    Added test case for orders

---
+++ netcore/tests/Koralium.TestFramework.Tests/OrderTests.cs
@@ -0,0 +1,38 @@
+﻿using Koralium.WebTests;
+using Koralium.WebTests.Entities.tpch;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Koralium.TestFramework.Tests
+{
+    public class OrderTests : QueryTest<Order>
+    {
+        private TestWebFactory testWebFactory;
+        private TpchData tpchData;
+        public override string TableName => "orders";
+
+        public override string GetTestWebUrl()
+        {
+            return testWebFactory.GetUrl();
+        }
+
+        protected override void OnSetup(IServiceCollection services)
+        {
+            testWebFactory = new TestWebFactory();
+            tpchData = new TpchData("../../../../../../TestData");
+            base.OnSetup(services);
+        }
+
+        protected override void OnTeardown()
+        {
+            testWebFactory.Stop();
+        }
+
+        public override IEnumerable<Order> TestData()
+        {
+            return tpchData.Orders;
+        }
+    }
+}
