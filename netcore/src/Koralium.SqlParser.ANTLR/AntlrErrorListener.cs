1a706ab903b5f2eefe5aeeab6fa666d3f120256a
Ulimo <alexander.ostman@hotmail.com>
2020-11-23 13:38:16 +0100

    Added error handling for sql parser.
    This fixes #77

---
+++ netcore/src/Koralium.SqlParser.ANTLR/AntlrErrorListener.cs
@@ -0,0 +1,23 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+using Koralium.SqlParser.Errors;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Koralium.SqlParser.ANTLR
+{
+    internal class AntlrErrorListener : BaseErrorListener
+    {
+        private readonly List<SqlParserError> _errors = new List<SqlParserError>();
+
+        public IReadOnlyList<SqlParserError> Errors => _errors;
+
+        public override void SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+        {
+            _errors.Add(new SqlParserError(msg));
+        }
+    }
+}
--- netcore/src/Koralium.SqlParser/Interfaces/ISqlParser.cs
+++ netcore/src/Koralium.SqlParser/Interfaces/ISqlParser.cs
@@ -1,4 +1,5 @@
-﻿using Koralium.SqlParser.Statements;
+﻿using Koralium.SqlParser.Errors;
+using Koralium.SqlParser.Statements;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +9,7 @@ namespace Koralium.SqlParser
     public interface ISqlParser
     {
         StatementList Parse(string text);
+
+        StatementList Parse(string text, out IReadOnlyList<SqlParserError> errors);
     }
 }
