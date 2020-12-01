e3cd0c9b50863aaf1c9ef70cd9328c49e8f68f6a
Ulimo <alexander.ostman@hotmail.com>
2020-12-02 00:27:29 +0100

    Added tests for json transport

---
+++ netcore/tests/Koralium.Transport.Json.Tests/JsonTests.cs
@@ -0,0 +1,75 @@
+using FluentAssertions;
+using Koralium.WebTests;
+using Koralium.WebTests.Entities.tpch;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Koralium.Transport.Json.Tests
+{
+    public class JsonTests
+    {
+        private class Response<T>
+        {
+            public List<T> Values { get; set; }
+        }
+
+        TestWebFactory webFactory;
+        HttpClient httpClient;
+        TpchData tpchData;
+        string url;
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            webFactory = new TestWebFactory();
+            httpClient = new HttpClient();
+            tpchData = new TpchData("../../../../../../TestData");
+            url = $"{webFactory.GetUrl()}/sql";
+        }
+
+        [Test]
+        public async Task TestPost()
+        {
+            var content = new StringContent("select * from customer");
+            var response = await httpClient.PostAsync(url, content);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var actual = Newtonsoft.Json.JsonConvert.DeserializeObject<Response<Customer>>(responseContent);
+
+            actual.Values.Should().Equal(tpchData.Customers);
+        }
+
+        [Test]
+        public async Task TestGet()
+        {
+            var response = await httpClient.GetAsync($"{url}?query=select * from customer");
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var actual = Newtonsoft.Json.JsonConvert.DeserializeObject<Response<Customer>>(responseContent);
+
+            actual.Values.Should().Equal(tpchData.Customers);
+        }
+
+        [Test]
+        public async Task TestGetMissingQueryParameter()
+        {
+            var response = await httpClient.GetAsync(url);
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            responseContent.Should().Be("Missing parameter 'query'");
+        }
+
+        [Test]
+        public async Task TestGetMultipleQueryParameter()
+        {
+            var response = await httpClient.GetAsync($"{url}?query=select * from customer&query=select * from customer");
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            responseContent.Should().Be("Only one parameter named 'query' can be sent in");
+        }
+
+    }
+}
---
+++ netcore/tests/Koralium.Transport.Json.Tests/TestWebFactory.cs
@@ -0,0 +1,48 @@
+﻿using Koralium.WebTests;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace Koralium.Transport.Json.Tests
+{
+    class TestWebFactory
+    {
+        readonly IHost host;
+
+        public TestWebFactory()
+        {
+            host = WebHostBuilder().Build(); //Create the server
+            host.Start();
+            AppContext.SetSwitch(
+                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+        }
+
+        private IHostBuilder WebHostBuilder()
+        {
+            return Host.CreateDefaultBuilder()
+                        .ConfigureWebHostDefaults(webBuilder =>
+                        {
+                            _ = webBuilder
+                            .ConfigureKestrel(c =>
+                            {
+                                c.Listen(IPEndPoint.Parse("0.0.0.0:5016"), l => l.Protocols = HttpProtocols.Http1);
+                            })
+                            .ConfigureAppConfiguration(opt =>
+                            {
+                                opt.AddJsonFile("testappsettings.json");
+                            })
+                            .UseStartup<Startup>();
+                        });
+        }
+
+        public string GetUrl()
+        {
+            return "http://127.0.0.1:5016";
+        }
+    }
+}
