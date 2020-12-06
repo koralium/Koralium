11c599dafaa9a6d16fb9db2a211d9dab63c74402
Ulimo <alexander.ostman@hotmail.com>
2020-12-06 19:10:29 +0100

    Added printing possibility of the sql tree.
    This fixes #95

---
+++ netcore/src/Koralium.SqlParser/Extensions/SqlNodeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Koralium.SqlParser.Visitor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Koralium.SqlParser
+{
+    public static class SqlNodeExtensions
+    {
+        public static string Print(this SqlNode sqlNode)
+        {
+            PrintVisitor printVisitor = new PrintVisitor();
+            return printVisitor.Print(sqlNode);
+        }
+    }
+}
