3eea60a38f1ec8fcb9b57a03afc61137fda5c90a
Ulimo <alexander.ostman@hotmail.com>
2021-01-02 19:01:33 +0100

    Added typescript client for the json transport protocol
    This fixes #150

---
+++ typescript-client/json/src/client.ts
@@ -0,0 +1,66 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { QueryOptions } from "./queryOptions";
+import { Metadata, QueryResult } from "./queryResult";
+
+import axios from 'axios'
+
+
+export class KoraliumClient {
+
+  private url: string;
+
+  constructor(url: string) {
+    this.url = url;
+  }
+
+  async queryScalar(sql: string, parameters: {} | null = null, headers: {} = {}): Promise<any> {
+
+    return this.query(sql, {
+      headers: headers,
+      parameters: parameters
+    })
+      .then(result => {
+        if(result.rows.length == 0) {
+          return null;
+        }
+
+        const firstRow = result.rows[0] as {[key: string]: any;};
+        return firstRow[Object.keys(firstRow)[0]];
+      });
+  }
+
+  async query(sql: string, queryOptions?: QueryOptions): Promise<QueryResult> {
+
+    const headers: {[key: string]: any;} = {
+      ...queryOptions?.headers
+    };
+
+    if(queryOptions?.parameters != null) {
+      for (let [key, value] of Object.entries(queryOptions.parameters)) {
+        headers["P_" + key] = value;
+      }
+    }
+    
+    return axios.post(this.url, sql, {
+      headers: headers
+    })
+      .then(response => {
+        return new QueryResult(response.data.values, new Metadata({}));
+      })
+      .catch(error => {
+          throw error;
+      });
+  }
+}
---
+++ typescript-client/json/tests/client.test.ts
@@ -0,0 +1,95 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { KoraliumClient } from "../src/client"
+import { QueryServer } from "./queryserver"
+import TpchData from "./tpchdata";
+
+//Basic jwt token that is used just for testing
+const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlRlc3QgdGVzdCIsInVuaXF1ZV9uYW1lIjoidGVzdCIsImlhdCI6MTUxNjIzOTAyMn0.aF80OiteMckPhSQcAL549V4AcyKHJA8LUs4mhzBnf2w";
+
+var client: KoraliumClient;
+var server: QueryServer;
+var tpchData: TpchData;
+
+jest.setTimeout(30000);
+
+beforeAll(async () => {
+  //server = new QueryServer();
+  //await server.start();
+
+  tpchData = new TpchData();
+  await tpchData.load();
+
+  client = new KoraliumClient(`http://127.0.0.1:5015/sql`);//new KoraliumClient(`${server.getIpAddress()}:${server.getPort()}`);
+});
+
+afterAll(async () => {
+  //await server.stop();
+})
+
+test("Get orders", async () => {
+  const expected = tpchData.getOrders();
+  const results = await client.query("select orderkey, custkey, orderstatus, totalprice, orderdate, orderpriority, clerk, shippriority, comment from orders");
+  expect(results.rows).toEqual(expected);
+});
+
+test("Get secure data", async () => {
+  const expected = tpchData.getOrders();
+  const results = await client.query(
+    "select orderkey, custkey, orderstatus, totalprice, orderdate, orderpriority, clerk, shippriority, comment from secure",
+    {headers: { Authorization: `Bearer ${accessToken}` }}
+  );
+    
+  expect(results.rows).toEqual(expected);
+});
+
+test("Test limit", async () => {
+  const expected = tpchData.getOrders().slice(0, 100);
+  const results = await client.query("select orderkey, custkey, orderstatus, totalprice, orderdate, orderpriority, clerk, shippriority, comment from orders limit 100");
+  expect(results.rows).toEqual(expected);
+});
+
+test("Test limit with parameter", async () => {
+  const expected = tpchData.getOrders().slice(0, 100);
+  const results = await client.query(
+    "select orderkey, custkey, orderstatus, totalprice, orderdate, orderpriority, clerk, shippriority, comment from orders limit @limit",
+    {parameters: { limit: 100 }}
+  );
+  expect(results.rows).toEqual(expected);
+});
+
+test("filter on date in parameter", async () => {
+  const expected = tpchData.getOrders().filter(x => new Date(x.orderdate) >= new Date("1993-01-01"));
+  const results = await client.query(
+    "select orderkey, custkey, orderstatus, totalprice, orderdate, orderpriority, clerk, shippriority, comment from orders where Orderdate >= @date",
+    { parameters: { date: '1993-01-01'}}
+  );
+
+  expect(results.rows).toEqual(expected);
+});
+
+test("select single field", async () => {
+  const expected = tpchData.getOrders().map(x =>  { return { orderkey: x.orderkey } });
+  const results = await client.query(
+    "select orderkey from orders"
+  );
+  expect(results.rows).toEqual(expected);
+});
+
+test("query scalar count", async () => {
+  const expected = tpchData.getOrders().length;
+  const result = await client.queryScalar("select count(*) from orders");
+
+  expect(result).toEqual(expected);
+})
